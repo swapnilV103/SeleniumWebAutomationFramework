d0880edced5076b8a25c539ff6106d555f1240d5
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-07-10 13:00:30 +0530

    Added more enhancements in base java file instead of static webDriver we created an dynamic driver seperate for each instance using ThreadLocal. Added steps of execution into report file. BeforeMethod And AfterMethod are created for browser initialization and quitting

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -1,4 +1,7 @@
 package Base;
+import java.lang.reflect.Method;
+
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -7,43 +10,64 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class BaseTest {
-	
+
 	protected ExtentReports extent; //used to initialize and configure reports
-	protected ExtentTest test; //it will add each step of the testcase
+	protected ExtentTest logger; //it will add/log each step of the testcase but it is different than a log
 	private ExtentSparkReporter testreporter; //to create the actual html report file
-	
 
-	
+	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-@BeforeTest
-public Void beforeTestMethod() {
-	
-	testreporter = new ExtentSparkReporter(System.getProperty("user.dir")+ "/reports/ExtentReport.html");
-	testreporter.config().setDocumentTitle("Test Automation Report");
-	testreporter.config().setReportName("Selenium Web Automation Framework Report");
-	testreporter.config().setTheme(Theme.DARK);
-	
-	
-	//protected ExtentReports extent = new ExtentReports();  This is actually the object of the class which could have been defined this way but we did by declaring variables first
-	extent = new ExtentReports();
-	extent.attachReporter(testreporter);
-	
-	//System Information
-	extent.setSystemInfo("OS", "Windows");
-	extent.setSystemInfo("Automation QA", "Swapnil Vidhate");
-	
-	return null;
-	
-	
-	
-}
 
-@BeforeMethod
-@AfterTest
-@AfterMethod
-public Void aftertestmethod() {
-	return null;
-	
-}
+
+
+	@BeforeTest
+	public void beforeTestMethod() 
+	{
+
+		testreporter = new ExtentSparkReporter(System.getProperty("user.dir")+ "/reports/ExtentReport.html");
+		testreporter.config().setDocumentTitle("Test Automation Report");
+		testreporter.config().setReportName("Selenium Web Automation Framework Report");
+		testreporter.config().setTheme(Theme.DARK);
+
+
+		//protected ExtentReports extent = new ExtentReports();  This is actually the object of the class; which could have been defined this way but we did by declaring variables first
+		extent = new ExtentReports();
+		extent.attachReporter(testreporter);
+
+		//System Information
+		extent.setSystemInfo("OS", "Windows");
+		extent.setSystemInfo("Automation QA", "Swapnil Vidhate");	
+
+	}
+
+	@BeforeMethod
+	@Parameters("browser")
+
+	public void beforeMethodMethod(String browser, Method testMethod)
+	{
+		WebDriver webDriver = WebDriverFactory.createDriver(browser);
+		logger = extent.createTest(testMethod.getName());
+		driver.set(webDriver);
+	}
+
+
+	public WebDriver getDriver() 
+	{
+		return driver.get();
+	}
+
+
+
+
+
+	@AfterTest
+	@AfterMethod
+	public void aftertestmethod() 
+	{	
+		extent.flush();
+		getDriver().quit();
+		driver.remove();
+
+	}
 
 }
