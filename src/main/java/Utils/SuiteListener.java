3e76cd2a502784ec831aa04a361e824dd07d0d62
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-07-10 18:16:35 +0530

    Created a listener when test case fails, it'll take screenshots and paste it there or will store in screenshots folder

---
+++ src/main/java/Utils/SuiteListener.java
@@ -0,0 +1,36 @@
+package Utils;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.IAnnotationTransformer;
+
+import Base.BaseTest;
+
+public class SuiteListener implements ITestListener,IAnnotationTransformer{
+
+	public void onTestFailure(ITestResult result) {
+		String filename = System.getProperty("user.dir")+File.separator+"screenshots"+File.separator+result.getMethod().getMethodName();
+		
+		TakesScreenshot ts = (TakesScreenshot)BaseTest.getDriver();//Cast to TakesScreenshot
+		File f1 = ts.getScreenshotAs(OutputType.FILE);// Get the screenshot file
+		
+		//File f1 = ((TakesScreenshot)BaseTest.driver).getScreenshotAs(OutputType.FILE);
+	
+		
+		try {
+			FileUtils.copyFile(f1, new File(filename+".png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
