d14169b9293d010241c649778f36d947ec364850
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-07-10 13:03:16 +0530

    created another class WebDriverFactory for dynamic driver picking. here also used the bonigarcia import which allows user to run the test using WebDriver Manager even if the driver and Browser version are mismatch.

---
+++ src/main/java/Base/WebDriverFactory.java
@@ -0,0 +1,35 @@
+package Base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class WebDriverFactory {
+
+    public static WebDriver createDriver(String browser) {
+        WebDriver driver;
+        switch (browser.toLowerCase()) {
+            case "chrome":
+            	WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+            	WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+            	WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            case "safari":
+                driver = new SafariDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Browser \"" + browser + "\" not supported.");
+        }
+        return driver;
+    }
+}
