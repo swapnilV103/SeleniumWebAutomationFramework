d21414fa9300fa93d8994e4137ab695cc5007ea8
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-07-24 01:35:14 +0530

    First test case file is created and couple of testcases added

---
+++ src/test/java/qa/tests/Testcase1.java
@@ -0,0 +1,24 @@
+package qa.tests;
+
+import org.testng.annotations.Test;
+
+import Base.BaseTest;
+import Utils.ElementFetch;
+import pageEvent.HomePageEvents;
+import pageEvent.LoginPageEvents;
+
+public class Testcase1 extends BaseTest{
+	
+	ElementFetch ele = new ElementFetch();
+	HomePageEvents homePage = new HomePageEvents();
+	LoginPageEvents loginPage = new LoginPageEvents();
+	
+  @Test
+  
+  public void Verify_if_Login_Works_Using_Correct_Credintials() {
+	  homePage.clickSignInButton();
+	  loginPage.VerifyIfLoginPageLoaded();
+	  loginPage.enterCredentials();
+	  
+  }
+}
