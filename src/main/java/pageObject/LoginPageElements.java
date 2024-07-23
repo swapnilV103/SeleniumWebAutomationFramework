3f1d707427bab7fbd99aeb387deb4c426b408958
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-07-24 01:30:27 +0530

    Creted an interface loginpage where all the locators are saved for login page

---
+++ src/main/java/pageObject/LoginPageElements.java
@@ -0,0 +1,13 @@
+package pageObject;
+
+public interface LoginPageElements {
+	
+	String loginpagetext = "//div[@class='ui fluid large blue submit button']";
+	String emailAddereddField = "//input[@placeholder='Email']";
+	String passwordField = "//input[@placeholder='Password']";
+	
+	
+	
+	
+
+}
