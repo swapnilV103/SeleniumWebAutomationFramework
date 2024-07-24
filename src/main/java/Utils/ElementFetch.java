83ab867235d0fcc92b393624ce0d48aa4a748c54
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-07-25 00:41:12 +0530

    changed getDriver methos to just driver

--- src/main/java/Utils/ElementFetch.java
+++ src/main/java/Utils/ElementFetch.java
@@ -14,19 +14,19 @@ public class ElementFetch {
 		switch(identifierType) {
 		
 		case "xpath":
-			return BaseTest.getDriver().findElement(By.xpath(identifierValue));
+			return BaseTest.driver.findElement(By.xpath(identifierValue));
 		
 		case "css":
-			return BaseTest.getDriver().findElement(By.cssSelector(identifierValue));
+			return BaseTest.driver.findElement(By.cssSelector(identifierValue));
 			
 		case "id":
-			return BaseTest.getDriver().findElement(By.id(identifierValue));
+			return BaseTest.driver.findElement(By.id(identifierValue));
 			
 		case "name":
-			return BaseTest.getDriver().findElement(By.partialLinkText(identifierValue));
+			return BaseTest.driver.findElement(By.partialLinkText(identifierValue));
 			
 		case "tagname":
-			return BaseTest.getDriver().findElement(By.tagName(identifierValue));
+			return BaseTest.driver.findElement(By.tagName(identifierValue));
 			
 		    default :
 		    	return null;	
@@ -40,19 +40,19 @@ public List<WebElement> getWebElements(String identifierType, String identifierV
 		switch(identifierType) {
 		
 		case "xpath":
-			return BaseTest.getDriver().findElements(By.xpath(identifierValue));
+			return BaseTest.driver.findElements(By.xpath(identifierValue));
 		
 		case "css":
-			return BaseTest.getDriver().findElements(By.cssSelector(identifierValue));
+			return BaseTest.driver.findElements(By.cssSelector(identifierValue));
 			
 		case "id":
-			return BaseTest.getDriver().findElements(By.id(identifierValue));
+			return BaseTest.driver.findElements(By.id(identifierValue));
 			
 		case "name":
-			return BaseTest.getDriver().findElements(By.partialLinkText(identifierValue));
+			return BaseTest.driver.findElements(By.partialLinkText(identifierValue));
 			
 		case "tagname":
-			return BaseTest.getDriver().findElements(By.tagName(identifierValue));
+			return BaseTest.driver.findElements(By.tagName(identifierValue));
 			
 		    default :
 		    	return null;	
