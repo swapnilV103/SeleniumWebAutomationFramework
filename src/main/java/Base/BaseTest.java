231e69e7a2839b704db2769c1b6005e7eebac9e2
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-08-20 23:29:57 +0530

    new comments

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -36,7 +36,9 @@ public class BaseTest {
         testreporter.config().setDocumentTitle("Test Automation Report");
         testreporter.config().setReportName("Selenium Web Automation Framework Report");
         testreporter.config().setTheme(Theme.DARK);
-
+        
+        
+        //protected ExtentReports extent = new ExtentReports();  This is actually the object of the class; which could have been defined this way but we did by declaring variables first
         extent = new ExtentReports();
         extent.attachReporter(testreporter);
 
